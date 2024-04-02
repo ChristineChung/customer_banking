436a32e92c7458868d468bb3be7ae8975a4ebb2a
Christine Chung <christine@fueled.com>
2024-04-02 16:04:56 -0400

    after review withing steph

--- customer_banking.py
+++ customer_banking.py
@@ -15,20 +15,21 @@ def main():
     balance = float(input('What is your balance? '))    
     interest = float(input('What is your interest rate? '))
     months = int(input('How many months? '))
-    new_balance = balance + interest_earned
+    
+    #new_balance = balance + interest_earned
 
     # Call the create_savings_account function and pass the variables from the user.
     balance, interest_earned = create_savings_account(balance, interest, months)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"\nSavings Account Balance after interest: ${new_balance:.2f}")
+    print(f"\nSavings Account Balance after interest: ${balance:.2f}")
     print(f"Interest Earned on Savings Account: ${interest_earned:.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
     print("\nEnter CD Account Details:")
-    cd_account = float(input("Enter initial balance: "))
+    cd_account = (input("Enter initial balance: "))
     cd_interest_rate = float(input("Enter interest rate: "))
     cd_months = int(input("Enter CD term length in months: "))
 
